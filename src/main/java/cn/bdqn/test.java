45163b00bf4a80f5b031095c8bb1257085532b14
laoli <asdasd@qq.com>
2023-02-03 19:48:18 +0800

    fenzhi.0.0.5

--- src/main/java/cn/bdqn/test.java
+++ src/main/java/cn/bdqn/test.java
@@ -2,6 +2,6 @@ package cn.bdqn;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("git test masiter");
+        System.out.println("git test fenzhi1");
     }
 }
